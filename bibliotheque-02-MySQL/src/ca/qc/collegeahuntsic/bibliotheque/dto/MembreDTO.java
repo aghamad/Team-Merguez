21fc98bd8a74051e7e62050e1cee4ca7cf38796d
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-09-12 17:59:30 -0400

    Deplacement et j'ai rename les nom de DTO

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
@@ -1,6 +1,14 @@
-
 package ca.qc.collegeahuntsic.bibliotheque.dto;
+/**
+ * Permet de représenter un tuple de la table membre.
+ * 
+ */
 
 public class MembreDTO {
 
+  public int    idMembre;
+  public String nom;
+  public long   telephone;
+  public int    limitePret;
+  public int    nbPret;
 }
