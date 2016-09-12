bf46ff0bd72726b8a98584edfdf0f538057ddb88
aghamad.AhmadAgha <aghamad.201467122@collegeahuntsic.qc.ca>
2016-09-12 16:33:38 -0400

    Ajout du package, folder: lib, resources et src

---
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Auteur : Ahmad Agha
+// Date de création : 2016-09-12
+/**
+ * TODO Auto-generated class javadoc
+ *
+ * @author Ahmad Agha
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque;
