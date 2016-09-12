524e50cb78c64923d0df52479b52b33e0848b8e2
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-09-12 15:28:12 -0400

    SQL2

---
+++ bibliotheque-02-MySQL/CreerBD.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *<pre>
+ *
+ *Permet de cr�er la BD utilis�e par Biblio.java.
+ *
+ *Param�tres:0- serveur SQL
+ *           1- bd nom de la BD
+ *           2- user id pour �tablir une connexion avec le serveur SQL
+ *           3- mot de passe pour le user id
+ *</pre>
+ */
+class CreerBD {
+    public static void main(String args[]) throws Exception,
+        SQLException,
+        IOException {
+
+        if(args.length < 3) {
+            System.out.println("Usage: java CreerBD <serveur> <bd> <user> <password>");
+            return;
+        }
+
+        Connexion cx = new Connexion(args[0],
+            args[1],
+            args[2],
+            args[3]);
+
+        Statement stmt = cx.getConnection().createStatement();
+
+        stmt.executeUpdate("DROP TABLE IF EXISTS reservation CASCADE");
+
+        stmt.executeUpdate("DROP TABLE IF EXISTS livre CASCADE");
+
+        stmt.executeUpdate("DROP TABLE IF EXISTS membre CASCADE");
+
+        stmt.executeUpdate("CREATE TABLE membre "
+            + "( "
+            + "idMembre INTEGER(3) CHECK(idMembre > 0), "
+            + "nom VARCHAR(10) NOT NULL,"
+            + "telephone BIGINT(10) , "
+            + "limitePret INTEGER(2) CHECK(limitePret > 0 AND limitePret <= 10) , "
+            + "nbpret INTEGER(2) DEFAULT 0 CHECK(nbpret >= 0) , "
+            + "CONSTRAINT cleMembre PRIMARY KEY (idMembre), "
+            + "CONSTRAINT limiteNbPret CHECK(nbpret <= limitePret) "
+            + ")");
+
+        stmt.executeUpdate("CREATE TABLE livre "
+            + "( "
+            + "idLivre INTEGER(3) CHECK(idLivre > 0) , "
+            + "titre VARCHAR(10) NOT NULL, "
+            + "auteur VARCHAR(10) NOT NULL,"
+            + "dateAcquisition DATE NOT NULL,"
+            + "idMembre INTEGER(3) , "
+            + "datePret DATE , "
+            + "CONSTRAINT cleLivre PRIMARY KEY (idLivre), "
+            + "CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre(idMembre) "
+            + ")");
+
+        stmt.executeUpdate("CREATE TABLE reservation "
+            + "( "
+            + "idReservation INTEGER(3) , "
+            + "idMembre INTEGER(3) , "
+            + "idLivre INTEGER(3) , "
+            + "dateReservation DATE , "
+            + "CONSTRAINT cleReservation PRIMARY KEY (idReservation) , "
+            + "CONSTRAINT cleCandidateReservation UNIQUE (idMembre,idLivre) ,"
+            + "CONSTRAINT refReservationMembre FOREIGN KEY (idMembre) REFERENCES membre(idMembre) ON DELETE CASCADE , "
+            + "CONSTRAINT refReservationLivre FOREIGN KEY (idLivre) REFERENCES livre(idLivre) ON DELETE CASCADE "
+            + ")");
+
+        stmt.close();
+        cx.fermer();
+    }
+}
