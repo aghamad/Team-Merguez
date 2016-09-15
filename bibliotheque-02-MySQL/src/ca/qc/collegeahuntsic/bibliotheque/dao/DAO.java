74cf29679b9e26ddec7c1fd302166abb9a7aa20f
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-09-15 12:12:21 -0400

    J'ai fait la classe DAO: ajout du seralizable + constructor vide

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/DAO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/DAO.java
@@ -1,6 +1,27 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.dao;
 
-public class DAO {
+import java.io.Serializable;
 
+import com.mysql.jdbc.Connection;
+
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+
+public class DAO implements Serializable {
+	
+
+
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	public DAO(Connexion connexion) {
+		// TODO Auto-generated constructor stub
+		super();
+		//setConnexion(connexion);
+	}
+	/*
+	protected Connection getConnection(){
+		return getConnexion().getConnection();
+	}*/
 }
