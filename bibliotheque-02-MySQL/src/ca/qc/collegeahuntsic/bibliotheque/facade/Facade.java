c34a93d6e079e6cd056b27e4984204bafe911583
Sasha Benjamin <201431829@collegeahuntsic.qc.ca>
2016-09-15 12:13:43 -0400

    Classe de base pour utils & facade

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/Facade.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/Facade.java
@@ -1,6 +1,12 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade;
 
-public class Facade {
+import java.io.Serializable;
 
+public class Facade implements Serializable {
+    public static final float serialVersionUID = 2L;
+
+    public Facade() {
+        super();
+    }
 }
---
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/Util.java
@@ -0,0 +1,13 @@
+
+package ca.qc.collegeahuntsic.bibliotheque.util;
+
+import java.io.Serializable;
+
+public class Util implements Serializable {
+    public static final float serialVersionUID = 2L;
+
+    public Util() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+}
