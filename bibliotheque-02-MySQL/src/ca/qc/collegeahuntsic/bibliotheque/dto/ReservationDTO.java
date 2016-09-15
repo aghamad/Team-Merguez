2b36515816791e6e523e6337c0cfdeb614dac4f2
aghamad.AhmadAgha <aghamad.201467122@collegeahuntsic.qc.ca>
2016-09-15 13:10:29 -0400

    Mettre privé les varaibles de la classe ReservationDTO

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/ReservationDTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/ReservationDTO.java
@@ -1,18 +1,56 @@
 // Fichier ResevationDTO.java
 // Auteur: Abdel Lee h.
-// Date de création : 15-09-2016
+// Date de crï¿½ation : 15-09-2016
+
 package ca.qc.collegeahuntsic.bibliotheque.dto;
+
 import java.sql.Date;
 
 /**
  * Permet de reprï¿½senter un tuple de la table membre.
- * 
+ *
  */
 
 public class ReservationDTO {
 
-  public int    idReservation;
-  public int    idLivre;
-  public int    idMembre;
-  public Date   dateReservation;
+    private int idReservation;
+
+    private int idLivre;
+
+    private int idMembre;
+
+    private Date dateReservation;
+
+    public int getIdReservation() {
+        return this.idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public int getIdLivre() {
+        return this.idLivre;
+    }
+
+    public void setIdLivre(int idLivre) {
+        this.idLivre = idLivre;
+    }
+
+    public int getIdMembre() {
+        return this.idMembre;
+    }
+
+    public void setIdMembre(int idMembre) {
+        this.idMembre = idMembre;
+    }
+
+    public Date getDateReservation() {
+        return this.dateReservation;
+    }
+
+    public void setDateReservation(Date dateReservation) {
+        this.dateReservation = dateReservation;
+    }
+
 }
