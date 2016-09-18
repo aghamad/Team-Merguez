34c5e0cbd8bd58729cac4abf2cc380d72042d12c
Ahmad <agha97a@gmail.com>
2016-09-18 19:54:34 -0400

    Fixed les erreurs des classes LivreDTO et MembreDTO

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/LivreDTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/LivreDTO.java
@@ -1,59 +1,76 @@
 // Fichier LivreDTO.java
 // Auteur: Abdel Lee h.
-// Date de création : 15-09-2016
+// Date de crï¿½ation : 15-09-2016
+
 package ca.qc.collegeahuntsic.bibliotheque.dto;
+
 import java.sql.Date;
 
 /**
  * Permet de reprï¿½senter un tuple de la table livre.
- * 
+ *
 */
 
 public class LivreDTO {
 
-	private int    idLivre;
-	private String titre;
-	private String auteur;
-	private Date   dateAcquisition;
-	private int    idMembre;
-	private Date   datePret;
-  
-	public int getIdLivre() {
-		return idLivre;
-	}
-	public void setIdLivre(int idLivre) {
-		this.idLivre = idLivre;
-	}
-	public String getTitre() {
-		return titre;
-	}
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
-	public String getAuteur() {
-		return auteur;
-	}
-	public void setAuteur(String auteur) {
-		this.auteur = auteur;
-	}
-	public Date getDateAcquisition() {
-		return dateAcquisition;
-	}
-	public void setDateAcquisition(Date dateAcquisition) {
-		this.dateAcquisition = dateAcquisition;
-	}
-	public int getIdMembre() {
-		return idMembre;
-	}
-	public void setIdMembre(int idMembre) {
-		this.idMembre = idMembre;
-	}
-	public Date getDatePret() {
-		return datePret;
-	}
-	public void setDatePret(Date datePret) {
-		this.datePret = datePret;
-	}
-  
-  
+    private int idLivre;
+
+    private String titre;
+
+    private String auteur;
+
+    private Date dateAcquisition;
+
+    private int idMembre;
+
+    private Date datePret;
+
+    public int getIdLivre() {
+        return this.idLivre;
+    }
+
+    public void setIdLivre(int idLivre) {
+        this.idLivre = idLivre;
+    }
+
+    public String getTitre() {
+        return this.titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return this.auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public Date getDateAcquisition() {
+        return this.dateAcquisition;
+    }
+
+    public void setDateAcquisition(Date dateAcquisition) {
+        this.dateAcquisition = dateAcquisition;
+    }
+
+    public int getIdMembre() {
+        return this.idMembre;
+    }
+
+    public void setIdMembre(int idMembre) {
+        this.idMembre = idMembre;
+    }
+
+    public Date getDatePret() {
+        return this.datePret;
+    }
+
+    public void setDatePret(Date datePret) {
+        this.datePret = datePret;
+    }
+
 }
--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
@@ -1,50 +1,64 @@
 // Fichier MembreDTO.java
 // Auteur: Abdel Lee h.
-// Date de création : 15-09-2016
+// Date de crï¿½ation : 15-09-2016
+
 package ca.qc.collegeahuntsic.bibliotheque.dto;
+
 /**
- * Permet de représenter un tuple de la table membre.
- * 
+ * Permet de reprï¿½senter un tuple de la table membre.
+ *
  */
 
 public class MembreDTO {
 
-	private int    idMembre;
-	private String nom;
-	private long   telephone;
-	private int    limitePret;
-	private int    nbPret;
-	  
-	public int getIdMembre() {
-		return idMembre;
-	}
-	public void setIdMembre(int idMembre) {
-		this.idMembre = idMembre;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public long getTelephone() {
-		return telephone;
-	}
-	public void setTelephone(long telephone) {
-		this.telephone = telephone;
-	}
-	public int getLimitePret() {
-		return limitePret;
-	}
-	public void setLimitePret(int limitePret) {
-		this.limitePret = limitePret;
-	}
-	public int getNbPret() {
-		return nbPret;
-	}
-	public void setNbPret(int nbPret) {
-		this.nbPret = nbPret;
-	}
-  
-  
+    private int idMembre;
+
+    private String nom;
+
+    private long telephone;
+
+    private int limitePret;
+
+    private int nbPret;
+
+    public int getIdMembre() {
+        return this.idMembre;
+    }
+
+    public void setIdMembre(int idMembre) {
+        this.idMembre = idMembre;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public long getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(long telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getLimitePret() {
+        return this.limitePret;
+    }
+
+    public void setLimitePret(int limitePret) {
+        this.limitePret = limitePret;
+    }
+
+    public int getNbPret() {
+        return this.nbPret;
+    }
+
+    public void setNbPret(int nbPret) {
+        this.nbPret = nbPret;
+    }
+
 }
