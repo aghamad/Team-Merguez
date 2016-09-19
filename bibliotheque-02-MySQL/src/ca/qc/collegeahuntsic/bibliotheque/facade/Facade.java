1accec586e8b9dc19ade83e1b029ef2e0aee5bbb
BattleDrifter <201396611@collegeahuntsic.qc.ca>
2016-09-19 17:16:25 -0400

    gestion d'erreur facade

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/Facade.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/Facade.java
@@ -6,12 +6,22 @@ package ca.qc.collegeahuntsic.bibliotheque.facade;
 
 import java.io.Serializable;
 
+/**
+ * Cette classe consiste a gérer les exceptions relié à la facade .
+ *
+ * @author Sasha Benjamin
+ */
 public class Facade implements Serializable {
     /**
-     * TODO Auto-generated field javadoc
+     * TODO Auto-generated field javadoc.
      */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Cet méthode consiste à retourner créer l'objet.
+     *
+     *
+     */
     public Facade() {
         super();
     }
