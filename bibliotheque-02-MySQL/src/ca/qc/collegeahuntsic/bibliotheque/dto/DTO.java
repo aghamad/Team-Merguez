8e4dc66eaef92159f5cf7b4f8683efbb1ca959e3
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-09-19 11:01:27 -0400

    Javadoc a jour  Nassim&Ahmed

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/DTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/DTO.java
@@ -1,21 +1,21 @@
 // Fichier DTO.java
 // Auteur: Abdel Lee h.
-// Date de création : 15-09-2016
+// Date de crï¿½ation : 15-09-2016
+
 package ca.qc.collegeahuntsic.bibliotheque.dto;
 
 import java.io.Serializable;
 
 public class DTO implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
-
-/**
- * 
- */
-		public DTO() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
+    /**
+     * 
+     */
+    public DTO() {
+        super();
+        // Auto-generated constructor stub
+    }
 
 }
