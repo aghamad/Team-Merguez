6de5b79c98a8a21cbea3ebcc582a1f1e3121f0ea
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-09-22 13:02:32 -0400

    reservation modif exception

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
@@ -5,6 +5,7 @@
 package ca.qc.collegeahuntsic.bibliotheque.service;
 
 import java.sql.Date;
+import java.sql.SQLException;
 import ca.qc.collegeahuntsic.bibliotheque.dao.LivreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.MembreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
@@ -12,7 +13,7 @@ import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
-import ca.qc.collegeahuntsic.bibliotheque.exception.BibliothequeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.ServiceException;
 
 /**
@@ -38,14 +39,14 @@ public class ReservationService extends Service {
       * @param livre Instance de la classe dao LivreDAO
       * @param membre Instance de la classe dao MembreDAO
       * @param reservation Instance de la classe dao ReservationDAO
-      * @throws BibliothequeException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+      * @throws ServiceException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
       */
     public ReservationService(LivreDAO livre,
         MembreDAO membre,
-        ReservationDAO reservation) throws BibliothequeException {
+        ReservationDAO reservation) throws ServiceException {
         if(livre.getConnexion() != membre.getConnexion()
             || reservation.getConnexion() != membre.getConnexion()) {
-            throw new BibliothequeException("Les instances de livre, de membre et de reservation n'utilisent pas la m�me connexion au serveur");
+            throw new ServiceException("Les instances de livre, de membre et de reservation n'utilisent pas la m�me connexion au serveur");
         }
         this.cx = livre.getConnexion();
         this.livre = livre;
@@ -60,30 +61,27 @@ public class ReservationService extends Service {
       * @param idLivre Le id d'un livre
       * @param idMembre Le id d'un membre
       * @param dateReservation La date de reservation d'un livre
-      * @throws ServiceException Une exception qui fournit des informations sur une erreur d'accès de base de données ou d'autres erreurs
-      * @throws BibliothequeException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
-      * @throws Exception Une exception qui fournit des informations sur une erreur vague
+      * @throws ServiceException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+     * @throws Exception Une exception qui fournit des informations sur une erreur vague
       */
     public void reserver(int idReservation,
         int idLivre,
         int idMembre,
-        String dateReservation) throws ServiceException,
-        BibliothequeException,
-        Exception {
+        String dateReservation) throws Exception {
         try {
             /* Verifier que le livre est pret� */
             final LivreDTO tupleLivre = this.livre.getLivre(idLivre);
             if(tupleLivre == null) {
-                throw new BibliothequeException("Livre inexistant: "
+                throw new ServiceException("Livre inexistant: "
                     + idLivre);
             }
             if(tupleLivre.getIdMembre() == 0) {
-                throw new BibliothequeException("Livre "
+                throw new ServiceException("Livre "
                     + idLivre
                     + " n'est pas prete");
             }
             if(tupleLivre.getIdMembre() == idMembre) {
-                throw new BibliothequeException("Livre "
+                throw new ServiceException("Livre "
                     + idLivre
                     + " deja prete a ce membre");
             }
@@ -91,18 +89,18 @@ public class ReservationService extends Service {
             /* V�rifier que le membre existe */
             final MembreDTO tupleMembre = this.membre.getMembre(idMembre);
             if(tupleMembre == null) {
-                throw new BibliothequeException("Membre inexistant: "
+                throw new ServiceException("Membre inexistant: "
                     + idMembre);
             }
 
             /* Verifier si date reservation >= datePret */
             if(Date.valueOf(dateReservation).before(tupleLivre.getDatePret())) {
-                throw new BibliothequeException("Date de reservation inferieure � la date de pret");
+                throw new ServiceException("Date de reservation inferieure � la date de pret");
             }
 
             /* V�rifier que la r�servation n'existe pas */
             if(this.reservation.existe(idReservation)) {
-                throw new BibliothequeException("R�servation "
+                throw new ServiceException("R�servation "
                     + idReservation
                     + " existe deja");
             }
@@ -127,25 +125,27 @@ public class ReservationService extends Service {
       * @param idReservation Le id d'une reservation
       * @param datePret La date d'acquisition du livre par le membre
       * @throws ServiceException Une exception qui fournit des informations sur une erreur d'accès de base de données ou d'autres erreurs
-      * @throws BibliothequeException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+      * @throws ServiceException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+     * @throws SQLException -
+     * @throws DAOException -
       * @throws Exception Une exception qui fournit des informations sur une erreur vague
       */
     public void prendreRes(int idReservation,
         String datePret) throws ServiceException,
-        BibliothequeException,
-        Exception {
+        SQLException,
+        DAOException {
         try {
             /* V�rifie s'il existe une r�servation pour le livre */
             final ReservationDTO tupleReservation = this.reservation.getReservation(idReservation);
             if(tupleReservation == null) {
-                throw new BibliothequeException("R�servation inexistante : "
+                throw new ServiceException("R�servation inexistante : "
                     + idReservation);
             }
 
             /* V�rifie que c'est la premi�re r�servation pour le livre */
             final ReservationDTO tupleReservationPremiere = this.reservation.getReservationLivre(tupleReservation.getIdLivre());
             if(tupleReservation.getIdReservation() != tupleReservationPremiere.getIdReservation()) {
-                throw new BibliothequeException("La r�servation n'est pas la premi�re de la liste "
+                throw new ServiceException("La r�servation n'est pas la premi�re de la liste "
                     + "pour ce livre; la premiere est "
                     + tupleReservationPremiere.getIdReservation());
             }
@@ -153,11 +153,11 @@ public class ReservationService extends Service {
             /* Verifier si le livre est disponible */
             final LivreDTO tupleLivre = this.livre.getLivre(tupleReservation.getIdLivre());
             if(tupleLivre == null) {
-                throw new BibliothequeException("Livre inexistant: "
+                throw new ServiceException("Livre inexistant: "
                     + tupleReservation.getIdLivre());
             }
             if(tupleLivre.getIdMembre() != 0) {
-                throw new BibliothequeException("Livre "
+                throw new ServiceException("Livre "
                     + tupleLivre.getIdLivre()
                     + " deja pr�t� � "
                     + tupleLivre.getIdMembre());
@@ -166,33 +166,33 @@ public class ReservationService extends Service {
             /* V�rifie si le membre existe et sa limite de pret */
             final MembreDTO tupleMembre = this.membre.getMembre(tupleReservation.getIdMembre());
             if(tupleMembre == null) {
-                throw new BibliothequeException("Membre inexistant: "
+                throw new ServiceException("Membre inexistant: "
                     + tupleReservation.getIdMembre());
             }
             if(tupleMembre.getNbPret() >= tupleMembre.getLimitePret()) {
-                throw new BibliothequeException("Limite de pr�t du membre "
+                throw new ServiceException("Limite de pr�t du membre "
                     + tupleReservation.getIdMembre()
                     + " atteinte");
             }
 
             /* Verifier si datePret >= tupleReservation.dateReservation */
             if(Date.valueOf(datePret).before(tupleReservation.getDateReservation())) {
-                throw new BibliothequeException("Date de pr�t inf�rieure � la date de r�servation");
+                throw new ServiceException("Date de pr�t inf�rieure � la date de r�servation");
             }
 
             /* Enregistrement du pret. */
             if(this.livre.preter(tupleReservation.getIdLivre(),
                 tupleReservation.getIdMembre(),
                 datePret) == 0) {
-                throw new BibliothequeException("Livre supprim� par une autre transaction");
+                throw new ServiceException("Livre supprim� par une autre transaction");
             }
             if(this.membre.preter(tupleReservation.getIdMembre()) == 0) {
-                throw new BibliothequeException("Membre supprim� par une autre transaction");
+                throw new ServiceException("Membre supprim� par une autre transaction");
             }
             /* Eliminer la r�servation */
             this.reservation.annulerRes(idReservation);
             this.cx.commit();
-        } catch(Exception e) {
+        } catch(DAOException e) {
             this.cx.rollback();
             throw e;
         }
@@ -203,23 +203,26 @@ public class ReservationService extends Service {
       * La r�servation doit exister.
       * @param idReservation Le id d'une reservation
       * @throws ServiceException Une exception qui fournit des informations sur une erreur d'accès de base de données ou d'autres erreurs
-      * @throws BibliothequeException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+      * @throws ServiceException Une exception qui fournit des informations sur une erreur de la bibliotheque ou d'autres erreurs
+     * @throws SQLException -
+     * @throws DAOException -
       * @throws Exception Une exception qui fournit des informations sur une erreur vague
       */
     public void annulerRes(int idReservation) throws ServiceException,
-        BibliothequeException,
-        Exception {
+
+        DAOException,
+        SQLException {
         try {
 
             /* V�rifier que la r�servation existe */
             if(this.reservation.annulerRes(idReservation) == 0) {
-                throw new BibliothequeException("R�servation "
+                throw new ServiceException("R�servation "
                     + idReservation
                     + " n'existe pas");
             }
 
             this.cx.commit();
-        } catch(Exception e) {
+        } catch(DAOException e) {
             this.cx.rollback();
             throw e;
         }
