2e3b8fafc7999881e332605ba8857fbd06d46a86
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-09-26 14:55:40 -0400

    fix

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
@@ -1,11 +1,11 @@
 // Fichier MembreDTO.java
 // Auteur: Abdel Lee h.
-// Date de cr�ation : 15-09-2016
+// Date de création : 15-09-2016
 
 package ca.qc.collegeahuntsic.bibliotheque.dto;
 
 /**
- * Permet de repr�senter un tuple de la table membre.
+ * Permet de représenter un tuple de la table membre.
  * @author [Team-Merguez]
  */
 
