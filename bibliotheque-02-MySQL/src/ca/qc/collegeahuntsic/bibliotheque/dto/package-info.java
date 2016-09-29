96faf54fee922c0fe0c9c03fcbf7dda665a90958
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-09-29 11:08:50 -0400

    commente

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/package-info.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/package-info.java
@@ -1,6 +1,10 @@
-/**.
- * package service
- * @author Team-Merguez
+// Fichier package-info.java
+// Auteur : Team Merguez
+// Date de création : 2016-09-12
+/**
+ * package-info de la librairie DTO.
+ *
+ * @author Team Merguez
  */
 
 package ca.qc.collegeahuntsic.bibliotheque.dto;
