e75f06cad4dcb3715b2914d58229c454cc610070
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-10-13 12:31:16 -0400

    J'ai complété la javadoc de la class PretDTO

--- bibliotheque-02-Oracle/src/ca/qc/collegeahuntsic/bibliotheque/dto/PretDTO.java
+++ bibliotheque-02-Oracle/src/ca/qc/collegeahuntsic/bibliotheque/dto/PretDTO.java
@@ -6,6 +6,7 @@ package ca.qc.collegeahuntsic.bibliotheque.dto;
 import java.sql.Timestamp;
 
 /**
+ * DTO de la table Pret.
  * @author Team-merguez
  *
  */
@@ -28,10 +29,8 @@ public class PretDTO extends DTO {
     /**
      * variable d'instance this.idLivre this.titre this.auteur this.dateAcquisition this.idMembre this.datePret.
      * @param idLivre id du livre
-     *
-     * @param titre titre du livre
-     * @param auteur auteur du livre
-     * @param dateAcquisition  la date de l'aquisition
+     * @param idPret pret du livre
+     * @param dateRetour  la date de retour
      * @param idMembre id du membre
      * @param datePret la date de pret
      */
@@ -74,42 +73,66 @@ public class PretDTO extends DTO {
         this.idLivre = idLivre;
     }
 
- 
+    /**
+     *  Getter de la variable d'instance <code>this.idPret</code>.
+     *  @return idPret La variable d'instance <code>this.idPret</code>
+     *  */
     public int getIdPret() {
-        return idPret;
+        return this.idPret;
     }
 
-
+    /**
+    *  Setter de la variable d'instance <code>this.idPret</code>.
+    *  @param idPret - La valeur à utiliser pour la variable d'instance <code>this.idPret</code>
+    *  */
     public void setIdPret(int idPret) {
         this.idPret = idPret;
     }
 
-
+    /**
+    *  Getter de la variable d'instance <code>this.idMembre</code>.
+    *  @return idMembre - La variable d'instance <code>this.idMembre</code>
+    *  */
     public int getIdMembre() {
-        return idMembre;
+        return this.idMembre;
     }
 
-
+    /**
+    *  Setter de la variable d'instance <code>this.idMembre</code>.
+    *  @param idMembre - La valeur à utiliser pour la variable d'instance <code>this.idMembre</code>
+    *  */
     public void setIdMembre(int idMembre) {
         this.idMembre = idMembre;
     }
 
-
+    /**
+    *  Getter de la variable d'instance <code>this.dateRetour</code>.
+    *  @return dateRetour - La variable d'instance <code>this.dateRetour</code>
+    *  */
     public Timestamp getDateRetour() {
-        return dateRetour;
+        return this.dateRetour;
     }
 
-
+    /**
+    *  Setter de la variable d'instance <code>this.dateRetour</code>.
+    *  @param dateRetour - La valeur à utiliser pour la variable d'instance <code>this.dateRetour</code>
+    *  */
     public void setDateRetour(Timestamp dateRetour) {
         this.dateRetour = dateRetour;
     }
 
-
+    /**
+    *  Getter de la variable d'instance <code>this.datePret</code>.
+    *  @return datePret - La variable d'instance <code>this.datePret</code>
+    *  */
     public Timestamp getDatePret() {
-        return datePret;
+        return this.datePret;
     }
 
-
+    /**
+    *  Setter de la variable d'instance <code>this.datePret</code>.
+    *  @param datePret - La valeur à utiliser pour la variable d'instance <code>this.datePret</code>
+    *  */
     public void setDatePret(Timestamp datePret) {
         this.datePret = datePret;
     }
