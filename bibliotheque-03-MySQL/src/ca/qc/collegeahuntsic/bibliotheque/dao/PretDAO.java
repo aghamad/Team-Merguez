237cfc6ebe76fdbcff8a0567b3b892f6038320ad
Ahmad Agha <aghamad.201467122@collegeahuntsic.qc.ca>
2016-10-13 12:17:55 -0400

    pret

---
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/PretDAO.java
@@ -0,0 +1,24 @@
+
+package ca.qc.collegeahuntsic.bibliotheque.dao;
+
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+
+/**
+ *
+ * TODO Auto-generated class javadoc
+ *
+ * @author Team-Merguez
+ */
+public class PretDAO extends DAO {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     * TODO Auto-generated constructor javadoc
+     *
+     * @param connexion
+     */
+    public PretDAO(Connexion connexion) {
+        super(connexion);
+    }
+}
