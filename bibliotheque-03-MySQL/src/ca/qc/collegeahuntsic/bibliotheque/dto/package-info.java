06252670e79fe797db0e91ef375e8cb86b1e968d
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-10-17 16:15:30 -0400

    Ajout des class dto du prof

--- bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/package-info.java
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dto/package-info.java
@@ -1,11 +1,11 @@
 // Fichier package-info.java
-// Auteur : Team-Merguez
+// Auteur : Gilles Bénichou
 // Date de création : 2016-05-18
 
 /**
  * Provides the DTO classes used with the bibliothèque.
  *
- * @author Team-Merguez
+ * @author Gilles Bénichou
  */
 
 package ca.qc.collegeahuntsic.bibliotheque.dto;
