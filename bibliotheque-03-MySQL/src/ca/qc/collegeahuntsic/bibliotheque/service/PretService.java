633c08f87cc167de7455b84bf253d2a87bb9f348
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-10-17 13:30:30 -0400

    ajout DAO dans pretDAO

--- bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/PretService.java
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/PretService.java
@@ -4,6 +4,11 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.service;
 
+import ca.qc.collegeahuntsic.bibliotheque.dao.LivreDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.MembreDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.PretDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
+
 /**
  * Service de la table <code>pret</code>.
  *
@@ -12,10 +17,98 @@ package ca.qc.collegeahuntsic.bibliotheque.service;
 public class PretService extends Service {
     private static final long serialVersionUID = 1L;
 
+    private PretDAO pretDAO;
+
+    private MembreDAO membreDAO;
+
+    private LivreDAO livreDAO;
+
+    private ReservationDAO reservationDAO;
+
     /**
      * Crée le service de la table <code>pret</code>.
      */
-    public PretService() {
+    public PretService(PretDAO pret,
+        MembreDAO membre,
+        LivreDAO livre,
+        ReservationDAO reservation) {
+
         super();
+        setPretDAO(pret);
+        setMembreDAO(membre);
+        setLivreDAO(livre);
+        setReservationDAO(reservation);
+    }
+
+    /**
+     * Getter de la variable d'instance <code>this.pretDAO</code>.
+     *
+     * @return La variable d'instance <code>this.pretDAO</code>
+     */
+    public PretDAO getPretDAO() {
+        return this.pretDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.pretDAO</code>.
+     *
+     * @param pretDAO La valeur à utiliser pour la variable d'instance <code>this.pretDAO</code>
+     */
+    public void setPretDAO(PretDAO pretDAO) {
+        this.pretDAO = pretDAO;
+    }
+
+    /**
+     * Getter de la variable d'instance <code>this.membreDAO</code>.
+     *
+     * @return La variable d'instance <code>this.membreDAO</code>
+     */
+    public MembreDAO getMembreDAO() {
+        return this.membreDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.membreDAO</code>.
+     *
+     * @param membreDAO La valeur à utiliser pour la variable d'instance <code>this.membreDAO</code>
+     */
+    public void setMembreDAO(MembreDAO membreDAO) {
+        this.membreDAO = membreDAO;
+    }
+
+    /**
+     * Getter de la variable d'instance <code>this.livreDAO</code>.
+     *
+     * @return La variable d'instance <code>this.livreDAO</code>
+     */
+    public LivreDAO getLivreDAO() {
+        return this.livreDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.livreDAO</code>.
+     *
+     * @param livreDAO La valeur à utiliser pour la variable d'instance <code>this.livreDAO</code>
+     */
+    public void setLivreDAO(LivreDAO livreDAO) {
+        this.livreDAO = livreDAO;
+    }
+
+    /**
+     * Getter de la variable d'instance <code>this.reservationDAO</code>.
+     *
+     * @return La variable d'instance <code>this.reservationDAO</code>
+     */
+    public ReservationDAO getReservationDAO() {
+        return this.reservationDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.reservationDAO</code>.
+     *
+     * @param reservationDAO La valeur à utiliser pour la variable d'instance <code>this.reservationDAO</code>
+     */
+    public void setReservationDAO(ReservationDAO reservationDAO) {
+        this.reservationDAO = reservationDAO;
     }
 }
