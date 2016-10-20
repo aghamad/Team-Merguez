612f38e8f2146ee2279199ac958786cf04a5143f
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-10-20 11:28:28 -0400

    Modification du script mysql

--- bibliotheque-03-MySQL/lib/MySQL.sql
+++ bibliotheque-03-MySQL/lib/MySQL.sql
@@ -2,21 +2,21 @@ DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS livre       CASCADE;
 DROP TABLE IF EXISTS membre      CASCADE;
 
-CREATE TABLE membre (idMembre   INTEGER(3)   CHECK (idMembre > 0),
+CREATE TABLE membre (idMembre   INTEGER(3) AUTO_INCREMENT  CHECK (idMembre > 0),
                      nom        VARCHAR(10)  NOT NULL,
                      telephone  BIGINT(10),
 					 limitePret INTEGER(2)   CHECK (limitePret > 0 AND limitePret <= 10),
 					 CONSTRAINT cleMembre    PRIMARY KEY (idMembre),
 					 CONSTRAINT limiteNbPret CHECK (nbPret <= limitePret));
 
-CREATE TABLE livre (idLivre         INTEGER(3)    CHECK (idLivre > 0),
+CREATE TABLE livre (idLivre         INTEGER(3) AUTO_INCREMENT   CHECK (idLivre > 0),
                     titre           VARCHAR(10)   NOT NULL,
 					auteur          VARCHAR(10)   NOT NULL,
 					dateAcquisition TIMESTAMP(3)  NOT NULL,
 					idMembre        INTEGER(3),
 					datePret        TIMESTAMP(3)  NULL,
 					CONSTRAINT      cleLivre      PRIMARY KEY (idLivre),
-					CONSTRAINT      refPretMembre FOREIGN KEY (idMembre) REFERENCES membre (idMembre));
+					CONSTRAINT      refMembre FOREIGN KEY (idMembre) REFERENCES membre (idMembre));
 					
 CREATE TABLE pret (idPret INTEGER(3) AUTO_INCREMENT CHECK(idPret > 0),
 					idMembre INTEGER(3) NOT NULL CHECK(idMembre > 0),
@@ -27,7 +27,7 @@ CREATE TABLE pret (idPret INTEGER(3) AUTO_INCREMENT CHECK(idPret > 0),
 					CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre(idMembre),
 					CONSTRAINT refPretLivre FOREIGN KEY (idLivre) REFERENCES livre(idLivre));
 
-CREATE TABLE reservation (idReservation   INTEGER(3),
+CREATE TABLE reservation (idReservation   INTEGER(3) AUTO_INCREMENT,
                           idMembre        INTEGER(3),
 						  idLivre         INTEGER(3),
 						  dateReservation TIMESTAMP(3),
