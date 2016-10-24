11b2ca326bda1a9eefacf378a24462f16d04d950
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2016-10-24 16:51:46 -0400

    sql et sql4

---
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/Service.java
@@ -0,0 +1,23 @@
+// Fichier Service.java
+// Auteur : Team-Merguez
+// Date de création : 2016-05-18
+
+package ca.qc.collegeahuntsic.bibliotheque.service;
+
+import java.io.Serializable;
+
+/**
+ * Classe de base pour tous les services.
+ *
+ * @author Team-Merguez
+ */
+public class Service implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructeur par défaut.
+     */
+    public Service() {
+        super();
+    }
+}
