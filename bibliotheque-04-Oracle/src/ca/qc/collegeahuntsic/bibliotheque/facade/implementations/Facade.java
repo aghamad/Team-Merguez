71dc92f2ae3e2fd3f770579b55e2881eca4adb64
Abdel Lee H. <201278908@collegeahuntsic.qc.ca>
2016-10-31 15:26:38 -0400

    Duplicate bibliotheque pour oracl version

---
+++ bibliotheque-04-Oracle/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/Facade.java
@@ -0,0 +1,21 @@
+// Fichier Facade.java
+// Auteur : Gilles Bénichou
+// Date de création : 2016-05-18
+
+package ca.qc.collegeahuntsic.bibliotheque.facade.implementations;
+
+import ca.qc.collegeahuntsic.bibliotheque.facade.interfaces.IFacade;
+
+/**
+ * Classe de base pour toutes les façades.
+ *
+ * @author Gilles Bénichou
+ */
+public class Facade implements IFacade {
+    /**
+     * Crée une façade.
+     */
+    protected Facade() {
+        super();
+    }
+}
