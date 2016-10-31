84f0cb5b63ba45aa9a5fa48d435570f588069ae2
Ahmad Agha <201467122@collegeahuntsic.qc.ca>
2016-10-31 15:45:28 -0400

    Pushed le Script Oracle

---
+++ bibliotheque -04-Oracle/MyOracle.sql
@@ -0,0 +1,58 @@
+DROP SEQUENCE SEQ_ID_RESERVATION;
+DROP SEQUENCE SEQ_ID_PRET;
+DROP SEQUENCE SEQ_ID_LIVRE;
+DROP SEQUENCE SEQ_ID_MEMBRE;
+
+DROP TABLE reservation CASCADE CONSTRAINTS PURGE;
+DROP TABLE pret CASCADE CONSTRAINTS PURGE; 
+DROP TABLE livre CASCADE CONSTRAINTS PURGE;
+DROP TABLE membre CASCADE CONSTRAINTS PURGE;
+
+CREATE SEQUENCE SEQ_ID_MEMBRE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE SEQ_ID_LIVRE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE SEQ_ID_PRET START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE SEQ_ID_RESERVATION START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE membre 
+(
+	idMembre NUMBER(3) CHECK(idMembre > 0), 
+	nom VARCHAR(100) NOT NULL,
+	telephone NUMBER(10) , 
+	limitePret NUMBER(2) CHECK(limitePret > 0 AND limitePret <= 10) , 
+	nbpret NUMBER(2) DEFAULT 0 CHECK(nbpret >= 0) , 
+	CONSTRAINT cleMembre PRIMARY KEY (idMembre), 
+	CONSTRAINT limiteNbPret check(nbpret <= limitePret)
+);
+
+CREATE TABLE livre 
+(
+	idLivre NUMBER(3) CHECK(idLivre > 0), 
+	titre VARCHAR(100) NOT NULL, 
+	auteur VARCHAR(100) NOT NULL, 
+	dateAcquisition TIMESTAMP NOT NULL, 
+	CONSTRAINT cleLivre PRIMARY KEY (idLivre)
+);
+
+CREATE TABLE pret 
+(
+	idPret NUMBER(3) CHECK(idPret > 0),
+	idMembre NUMBER(3) CHECK(idMembre > 0),
+	idLivre NUMBER(3) CHECK(idLivre > 0),
+	datePret TIMESTAMP(3),
+	dateRetour TIMESTAMP(3),
+	CONSTRAINT clePrimairePret PRIMARY KEY (idPret),
+	CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre(idMembre),
+	CONSTRAINT refPretLivre FOREIGN KEY (idLivre) REFERENCES livre(idLivre)
+);
+
+CREATE TABLE reservation 
+( 
+	idReservation NUMBER(3) CHECK(idReservation > 0), 
+	idMembre NUMBER(3) CHECK(idMembre > 0), 
+	idLivre NUMBER(3) CHECK(idLivre > 0), 
+	dateReservation TIMESTAMP(3) NOT NULL, 
+	CONSTRAINT cleReservation PRIMARY KEY (idReservation) , 
+	CONSTRAINT cleCandidateReservation UNIQUE (idMembre,idLivre) , 
+	CONSTRAINT refReservationMembre FOREIGN KEY (idMembre) REFERENCES membre(idMembre) ON DELETE CASCADE, 
+	CONSTRAINT refReservationLivre FOREIGN KEY (idLivre) REFERENCES livre(idLivre) ON DELETE CASCADE 
+);
