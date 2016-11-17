06d0d55c8e997382f29e365a09a90755e9ad4575
BattleDrifter <201396611@collegeahuntsic.qc.ca>
2016-11-17 11:44:59 -0500

    membre facade mise a jour

--- bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/MembreFacade.java
+++ bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/MembreFacade.java
@@ -4,15 +4,9 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.implementations;
 
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.facade.InvalidServiceException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
@@ -20,6 +14,7 @@ import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationE
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ServiceException;
 import ca.qc.collegeahuntsic.bibliotheque.facade.interfaces.IMembreFacade;
 import ca.qc.collegeahuntsic.bibliotheque.service.interfaces.IMembreService;
+import org.hibernate.Session;
 
 /**
  * Facade pour interagir avec le service de membres.
@@ -35,9 +30,9 @@ public class MembreFacade extends Facade implements IMembreFacade {
      * @param membreService Le service de la table <code>membre</code>
      * @throws InvalidServiceException Si le service de membre est <code>null</code>
      */
-    public MembreFacade(IMembreService membreService) throws InvalidServiceException {
+    MembreFacade(IMembreService membreService) throws InvalidServiceException {
         // TODO: Change the constructor visibility to package when switching to Spring
-        super();
+        super(membreService);
         if(membreService == null) {
             throw new InvalidServiceException("Le service de livres ne peut être null");
         }
@@ -69,13 +64,12 @@ public class MembreFacade extends Facade implements IMembreFacade {
      * {@inheritDoc}
      */
     @Override
-    public void inscrire(Connexion connexion,
+    public void inscrire(Session session,
         MembreDTO membreDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidDTOClassException,
         FacadeException {
         try {
-            getMembreService().inscrire(connexion,
+            getMembreService().inscrire(session,
                 membreDTO);
         } catch(ServiceException serviceException) {
             throw new FacadeException(serviceException);
@@ -87,19 +81,14 @@ public class MembreFacade extends Facade implements IMembreFacade {
      * {@inheritDoc}
      */
     @Override
-    public void desinscrire(Connexion connexion,
+    public void desinscrire(Session session,
         MembreDTO membreDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidDTOClassException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
         ExistingLoanException,
         ExistingReservationException,
         FacadeException {
         try {
-            getMembreService().desinscrire(connexion,
+            getMembreService().desinscrire(session,
                 membreDTO);
         } catch(ServiceException serviceException) {
             throw new FacadeException(serviceException);
